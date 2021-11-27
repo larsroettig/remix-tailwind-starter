820b588b10de36d208a9cb49981b24175ed6d3a7
Lars Roettig <l.roettig@techdivision.com>
2021-11-27 20:11:04 +0100

    Valdation

---
+++ app/components/__tests__/header.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import {render, screen} from '@testing-library/react'
+import Header from '../header'
+
+// !!!! Never write tests like this. It's basically useless
+// it's just here to make sure our testing setup works
+test('Renders Header', () => {
+    render(<Header />)
+    expect(screen.getByRole('heading', {name: /Remix Tailwind Starter/})).toBeInTheDocument()
+  })
---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <h1 className="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-8">
+                Remix Tailwind Starter
+            </h1>
+        </div>
+    )
+}
